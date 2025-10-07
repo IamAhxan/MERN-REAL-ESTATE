571a533a472fd4e8b6f3fa1e795246e63d8ab740
IamAhxan <mohammadahsan7744@gmail.com>
2025-10-07 19:30:15 +0500

    signup backend

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -4,4 +4,6 @@ export const test = (req, res) => {
 
 }
 
+
+
 export default test
--- api/index.js
+++ api/index.js
@@ -3,14 +3,18 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import connectDB from './configs/mongodb.connect.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config() //env
 
 const app = express(); //express
+app.use(express.json())
 
 connectDB() //connect Database mongoDB
 
 app.use('/api/user', userRouter) //User Router
+
+app.use("/api/auth", authRoutes)
 
 app.listen(3000, () => {
     console.log('server is running on port 3000!!!')
