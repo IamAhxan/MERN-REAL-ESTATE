9bcb2c07fe26e56c4b97b0a6e7189ad3c92d8121
IamAhxan <mohammadahsan7744@gmail.com>
2025-10-08 21:36:01 +0500

    Sign In API route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { signUp } from "../controllers/auth.controller.js";
+import { signup, signin } from "../controllers/auth.controller.js";
 
 
 const router = express.Router();
 
-router.post("/signup", signUp)
-
+router.post("/signup", signup)
+router.post("/signin", signin)
 
 
 export default router;
