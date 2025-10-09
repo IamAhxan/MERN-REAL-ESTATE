9645dfb6b5a15c8da8ab9edbb5d463b76f6f54bd
IamAhxan <mohammadahsan7744@gmail.com>
2025-10-09 21:01:03 +0500

    add redux

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import { RouterProvider } from 'react-router-dom'
 import routes from '../routes.jsx'
+import { store } from '../redux/store.js'
+import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Provider store={store}>
     <RouterProvider router={routes} />
+    </Provider>
   </StrictMode>,
 )
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import { Link,  useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart, signInSuccess, signInFailure } from '../../redux/user/userSlice';
 
 const SignIn = () => {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false); 
+  const {loading, error} = useSelector((state)=>state.user)
   const navigate = useNavigate()
+  const dispatch = useDispatch(); // add this line
+
 
   const handleChange = (e) => {
     setFormData({
@@ -17,8 +20,7 @@ const SignIn = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
-      setError(null); // ✅ clear old error before new submit
+      dispatch(signInStart())
 
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
@@ -31,18 +33,13 @@ const SignIn = () => {
       const data = await res.json();
 
       if (data.success === false) {
-        setError(data.message);
-        setLoading(false);
-        return;
+       return dispatch(signInFailure(data.message))
       }
-      setError(null)
-      setLoading(false);
+     dispatch(signInSuccess(data))
       navigate('/')
       console.log(data); 
     } catch (err) {
-      setLoading(false);
-      setError(err.message);
-      console.log(err.message);
+     dispatch(signInFailure(err.message))
     }
   };
 
