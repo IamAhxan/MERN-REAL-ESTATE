b75ef6efeb178832d65e77c84a8e83b81253f64a
IamAhxan <mohammadahsan7744@gmail.com>
2025-10-30 18:03:18 +0500

    mongoDB async/await

--- api/configs/mongodb.connect.js
+++ api/configs/mongodb.connect.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
-const connectDB = () => {
-   mongoose.connect(process.env.MONGO_URI).then(() => {
-       console.log('Connected to MongoDB!')
-   }).catch((err) => { console.log(err) })
+const connectDB = async () => {
+    await mongoose.connect(process.env.MONGO_URI).then(() => {
+        console.log('Connected to MongoDB!')
+    }).catch((err) => { console.log(err) })
 }
 
 export default connectDB;
